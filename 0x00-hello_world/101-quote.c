04416cce6ed7f6a9eefd48ff6d1ec3297694b763
Joy <sannijoy.omowunmi@gmail.com>
2022-09-11 07:49:43 -0700

    print with new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-	int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
 {
 	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
-
 }
