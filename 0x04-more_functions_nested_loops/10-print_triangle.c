287aad39934de248c9b215884e835d243e4a037e
Joy <sannijoy.omowunmi@gmail.com>
2022-09-16 11:28:19 -0700

    Write a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,13 +1,37 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * add - Adds two integers.
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
- *
- * Return: The result of the addition.
+ *print_triangle - prints a triangle
+ *@size:size parameter of triangle
+ *Return: returns nothing
  */
-int add(int num1, int num2)
+
+void print_triangle(int size)
 {
-	return (num1 + num2);
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 1; inc1 <= size; inc1++)
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				putchar(' ');
+			}
+
+			for (inc2 = 0; inc2 < inc1; inc2++)
+			{
+				putchar('#');
+			{
+
+			if (inc1 == size)
+			{
+				continue;
+			}
+
+			putchar('\n');
+		}
+	}
+	putchar('\n');
 }
