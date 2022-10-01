56ad200568b875964b11adc8c730459e94c0e10c
Joy <sannijoy.omowunmi@gmail.com>
2022-10-01 05:04:33 -0700

    create a static library libmy.a

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * _isdigit - functions that check for digit character
- * @c: the character
- * Return: 1 if c is uppecase overwise 0
- */
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
+if ((c >= 48) && (c <= 57))
+return (1);
 
-	return (0);
+return (0);
 }
--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,20 +1,15 @@
 #include "main.h"
-
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
  */
 int _strlen(char *s)
 {
-	int i = 0;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-	while  (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-
-	return (i);
+return (inc);
 }
