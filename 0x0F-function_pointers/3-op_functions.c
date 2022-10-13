e75754adae4bf3b60f10c09428ec331d93f56480
Joy <sannijoy.omowunmi@gmail.com>
2022-10-13 14:57:20 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -29,12 +29,12 @@ int op_sub(int a, int b)
 }
 
 /**
- *  * op_mul - multiplies a and b
- *   * @a: first number
- *    * @b: second number
- *     *
- *      * Return: multiplication of a and b
- *      */
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
 
 int op_mul(int a, int b)
 {
@@ -42,12 +42,12 @@ int op_mul(int a, int b)
 }
 
 /**
- *  * op_div - divides a by b
- *   * @a: first number
- *    * @b: second number
- *     *
- *      * Return: integer division of a by b
- *      */
+ * op_div - divides a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer division of a by b
+ */
 
 int op_div(int a, int b)
 {
@@ -60,12 +60,12 @@ int op_div(int a, int b)
 }
 
 /**
- *  * op_mod - find the remainder of the division of a by b
- *   * @a: first number
- *    * @b: second number
- *     *
- *      * Return: remainder of the division of a by b
- *      */
+ * op_mod - find the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
+ */
 
 int op_mod(int a, int b)
 {
